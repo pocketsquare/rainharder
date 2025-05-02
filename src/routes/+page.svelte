f0c86b3e22959b7f3ef604dc36a1c93be905b40b
pocketsquare <lesters@gmail.com>
2025-05-01 18:56:54 -0600

    ChatGPT is a loser that loses even at losing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Rain Harder</title>
+  <link rel="stylesheet" href="/styles.css">
+</svelte:head>
+
+<div class="container">
+  <h1>Hello, World!</h1>
+  <p>(Make it rain harder.)</p>
+</div>
