664e30d3a57bae4bd983ecb02e64a9a34f2255d2
pocketsquare <lesters@gmail.com>
2025-05-23 14:09:01 -0600

    more edits to /kidai

---
+++ src/routes/mistral/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    const title = "Mistral";
+    const pdfSrc = "/pdfs/mightier.pdf";
+  </script>
+  
+  <svelte:head>
+    <title>{title}</title>
+  </svelte:head>
+  
+  <!-- Explicit spacing to preserve header -->
+  <!-- <div style="padding-top:180px; width:95vw; margin:0 auto;">
+    <iframe
+      src={pdfSrc}
+      title="The Art of Dressing Fiscally PDF"
+      style="width:100%; height:85vh; border:none;">
+    </iframe>
+  </div>
+   -->
